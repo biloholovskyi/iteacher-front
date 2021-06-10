e800055597b92d5528b306e7977507a3cb0730a6
NazikDoker <skorbachnazik@gmail.com>
2021-06-10 14:36:56 +0300

    TabNotes

---
+++ src/pages/studentCart/tabs/tabCourses/tabCourses.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const TabCourses = () => {
+  return (
+    <div>
+      Курси
+    </div>
+  )
+}
+
+export default TabCourses
--- src/pages/studentCart/tabs/tabs.js
+++ src/pages/studentCart/tabs/tabs.js
@@ -1,18 +1,37 @@
-import React from 'react'
+import React, {useState} from 'react'
+
+import TabCourses from "./tabCourses/tabCourses";
+import TabNotes from "./tabNotes/tabNotes";
 
 import * as Style from './styled.js'
 
 const Tabs = () => {
+  const [activeTab, setActiveTab] = useState('courses')
+
+  const changeTab = (tab) => {
+    setActiveTab(tab)
+  }
+
   return (
     <>
       <Style.TabsHead>
-        <div className={'tabs active'}>
+        <div
+          className={`tabs ${activeTab === 'courses' && 'active'}`}
+          onClick={() => changeTab('courses')}
+        >
           Курсы
         </div>
-        <div className={'tabs'}>
+        <div
+          className={`tabs ${activeTab === 'notes' && 'active'}`}
+          onClick={() => changeTab('notes')}
+        >
           Заметки
         </div>
       </Style.TabsHead>
+
+      {
+        activeTab === 'courses' ? <TabCourses/> : <TabNotes/>
+      }
     </>
   )
 }
