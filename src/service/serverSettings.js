3dbdebf6997e26326a4341b3792269e5167ed593
Biloholovskyi <biloholovskyi>
2021-07-05 21:02:11 +0300

    save

--- src/service/serverSettings.js
+++ src/service/serverSettings.js
@@ -2,9 +2,8 @@
 const api_url = process.env.REACT_APP_API_URL;
 class ServerSettings {
   constructor() {
-    // this.api = 'http://dev4.i-teacher.online/';
+    // this.api = 'https://dev4.i-teacher.online/';
     this.api = api_url;
-
   }
 
   getApi = () => {
