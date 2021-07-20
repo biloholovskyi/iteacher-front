5b9170aeda9ca77fbf2eeed5d48e4feadb25730e
Biloholovskyi <biloholovskyi>
2021-07-20 15:58:09 +0300

    Fix bug create new schedule in connected student

--- src/pages/courseTemplate/planList/planList.js
+++ src/pages/courseTemplate/planList/planList.js
@@ -6,15 +6,17 @@ import {monthNames} from "../../../service/monthes";
 
 const PlanList = ({plans, openModal, course}) => {
   const elements = plans.map((lesson, key) => {
-    // получаем id урока и проверяем есть ли он в рассписание
-    const schedule = course.schedules.find(l => parseInt(l.lesson) === lesson.id);
     let timeString;
+    if(course.schedules) {
+      // получаем id урока и проверяем есть ли он в рассписание
+      const schedule = course.schedules.find(l => parseInt(l.lesson) === lesson.id);
 
-    if(schedule) {
-      const date = `${schedule.date.split('.')[0]} ${monthNames[parseInt(schedule.date.split('.')[1]) - 1]}`;
-      timeString = `${date}, ${schedule.time} - ${(parseInt(schedule.time.split(':')[0]) + 1) < 24 ? parseInt(schedule.time.split(':')[0]) + 1 : '00'}:00`;
-    } else {
-      timeString = '';
+      if(schedule) {
+        const date = `${schedule.date.split('.')[0]} ${monthNames[parseInt(schedule.date.split('.')[1]) - 1]}`;
+        timeString = `${date}, ${schedule.time} - ${(parseInt(schedule.time.split(':')[0]) + 1) < 24 ? parseInt(schedule.time.split(':')[0]) + 1 : '00'}:00`;
+      } else {
+        timeString = '';
+      }
     }
 
     return (
